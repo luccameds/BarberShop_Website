7ee34114561b90d7ce56f6e6f3f133f1978597a6
luccameds <luccameds@gmail.com>
2023-06-16 21:18:12 -0300

    Updated the Header.jsx to mobile device

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={'px-10 py-10 flex flex-row justify-between bg-[#F6E5BA]'}>
+      <img src="/logo.png" alt="logo" className="w-[60px]" />
+      <img src="/menu.png" alt="logo" className="w-[30px] object-contain" />
+    </div>
+  );
 };
 
 export default Header;
