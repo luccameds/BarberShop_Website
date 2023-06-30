b9826610cf357c6428dc2b038111d98ef5c6dac3
luccameds <luccameds@gmail.com>
2023-06-29 21:29:18 -0300

    Created index.js in constants

---
+++ constants/index.js
@@ -0,0 +1,40 @@
+const images = [
+  {
+    ID: 1,
+    imgURL: '/photo-1.png',
+  },
+  {
+    ID: 2,
+    imgURL: '/photo-2.png',
+  },
+  {
+    ID: 3,
+    imgURL: '/photo-3.png',
+  },
+  {
+    ID: 4,
+    imgURL: '/photo-4.png',
+  },
+];
+
+const imagesSocialMedia = [
+  {
+    ID: 1,
+    imgURL: '/icoinstagram.png',
+    linkURL: 'https://www.instagram.com/01dom_rafael/',
+  },
+  {
+    ID: 2,
+    imgURL: '/icofacebook.png',
+    linkURL: 'https://pt-br.facebook.com/domrafaelbarbearia/',
+  },
+  // { ID: 3, imgURL: '/icotwitter.png' },
+  {
+    ID: 3,
+    imgURL: '/icowhatsapp.png',
+    linkURL:
+      'https://api.whatsapp.com/send/?phone=556193373090&text=Olá, gostaria de marcar um horário.&type=phone_number&app_absent=0',
+  },
+];
+
+export { images, imagesSocialMedia };
